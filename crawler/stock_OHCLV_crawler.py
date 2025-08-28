f2994d20f66bfb336f315651eb43cdd79c250c8d
Huynh Thanh Trung <thanhtrunghuynh93@gmail.com>
2025-08-28 09:35:00 +0200

    update

--- crawler/stock_OHCLV_crawler.py
+++ crawler/stock_OHCLV_crawler.py
@@ -55,7 +55,7 @@ def load_data_direct(ticker, exchange = "HOSE", interval = "1W", nbars = 5000, m
 def crawl_OHCLV(ticker, exchange = "HOSE", interval = "1W", nbars = 5000, live = True):
     df = load_data_direct(ticker, exchange, interval, nbars)
     if live:
-        quote = Quote(symbol='ACB', source='VCI')
+        quote = Quote(symbol=ticker, source='VCI')
         last_price = quote.intraday(page_size = 1)["price"].values[-1] * 1000
         df.loc[df.index[-1], 'close'] = last_price
 
